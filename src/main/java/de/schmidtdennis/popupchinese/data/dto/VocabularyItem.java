152b0b919f70ca300c3173219dab755c99a7884f
dcnis <bgk.dennis@yahoo.de>
2020-02-15 23:07:50 +0100

    added JsonProperty

--- src/main/java/de/schmidtdennis/popupchinese/data/dto/VocabularyItem.java
+++ src/main/java/de/schmidtdennis/popupchinese/data/dto/VocabularyItem.java
@@ -2,6 +2,8 @@ package de.schmidtdennis.popupchinese.data.dto;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class VocabularyItem implements Serializable {
 
     public Integer id;
+    @JsonProperty("type")
     public String typ;
     public String pinyin; 
     public String chinese;
