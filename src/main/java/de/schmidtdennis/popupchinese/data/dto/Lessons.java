d6ed837f106ce3ea8400e87c54dd9ba5815902fa
dcnis <bgk.dennis@yahoo.de>
2020-02-17 08:45:43 +0100

    removed custom sequence

--- src/main/java/de/schmidtdennis/popupchinese/data/dto/Lessons.java
+++ src/main/java/de/schmidtdennis/popupchinese/data/dto/Lessons.java
@@ -13,8 +13,7 @@ import lombok.Data;
 public class Lessons {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "lessons_id_seq")
-    @SequenceGenerator(name = "lessons_id_seq", sequenceName = "lessons_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     public Integer id;
     public String title;
     public String discussion;
