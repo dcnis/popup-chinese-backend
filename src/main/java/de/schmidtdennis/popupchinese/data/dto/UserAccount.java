67ed92d712107d0eecc0a5e464f078d653337563
dcnis <bgk.dennis@yahoo.de>
2020-02-17 08:52:54 +0100

    deleted custom sequence generator from UserAccount

--- src/main/java/de/schmidtdennis/popupchinese/data/dto/UserAccount.java
+++ src/main/java/de/schmidtdennis/popupchinese/data/dto/UserAccount.java
@@ -21,8 +21,7 @@ public class UserAccount {
     }
 
     @Id
-    @GeneratedValue(generator = "user_account_id_seq", strategy = GenerationType.SEQUENCE)
-    @SequenceGenerator(name = "user_account_id_seq", sequenceName = "user_account_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE) 
     public Integer id;
     public String name;
     public String firstName;
