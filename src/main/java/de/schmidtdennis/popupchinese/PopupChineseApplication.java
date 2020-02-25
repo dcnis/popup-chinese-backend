b9ceae04d9271ae118dc27cbf8c90cb39e4bca63
dcnis <bgk.dennis@yahoo.de>
2020-02-25 16:47:50 +0100

    added cors() to WebSecurityConfigurerAdapter

--- src/main/java/de/schmidtdennis/popupchinese/PopupChineseApplication.java
+++ src/main/java/de/schmidtdennis/popupchinese/PopupChineseApplication.java
@@ -1,12 +1,18 @@
 package de.schmidtdennis.popupchinese;
 
+import java.util.Arrays;
+
 import com.okta.spring.boot.oauth.Okta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class PopupChineseApplication {
@@ -21,12 +27,24 @@ public class PopupChineseApplication {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
+                .cors()
+                .and()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
                 .oauth2ResourceServer().jwt();
                 
             // Send a 401 message to the browser (w/o this, you'll see a blank page)
             Okta.configureResourceServer401ResponseBody(http);
+        }
+
+        @Bean
+        CorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(Arrays.asList("https://heroku-popup-chinese-frontend.herokuapp.com/"));
+            configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            return source;
         }
     }
 
