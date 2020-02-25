4913951edc78587267947ea90d85017f155cb824
dcnis <bgk.dennis@yahoo.de>
2020-02-25 22:43:14 +0100

    refactoring config

--- src/main/java/de/schmidtdennis/popupchinese/PopupChineseApplication.java
+++ src/main/java/de/schmidtdennis/popupchinese/PopupChineseApplication.java
@@ -1,13 +1,8 @@
 package de.schmidtdennis.popupchinese;
 
-
-import com.okta.spring.boot.oauth.Okta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
@@ -17,24 +12,5 @@ public class PopupChineseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PopupChineseApplication.class, args);
 	}
-
-
-	@Configuration
-    static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-            .oauth2ResourceServer().jwt();
-
-            http.cors();
-
-            // force a non-empty response body for 401's to make the response more browser friendly
-            Okta.configureResourceServer401ResponseBody(http);
-
-        }
-    }
 
 }
