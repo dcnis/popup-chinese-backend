94b09bfc44084ba4ceb762076ad1f4d0f8dd6f31
dcnis <bgk.dennis@yahoo.de>
2020-02-25 16:49:52 +0100

    changed cors uri

--- src/main/java/de/schmidtdennis/popupchinese/PopupChineseApplication.java
+++ src/main/java/de/schmidtdennis/popupchinese/PopupChineseApplication.java
@@ -40,7 +40,7 @@ public class PopupChineseApplication {
         @Bean
         CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(Arrays.asList("https://heroku-popup-chinese-frontend.herokuapp.com/"));
+            configuration.setAllowedOrigins(Arrays.asList("https://heroku-popup-chinese-frontend.herokuapp.com"));
             configuration.setAllowedMethods(Arrays.asList("GET","POST"));
             UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
             source.registerCorsConfiguration("/**", configuration);
