168b8819a2ee3a571d5ff263788ca993deb14cae
dcnis <bgk.dennis@yahoo.de>
2020-02-29 11:24:20 +0100

    removed second EnableGlobalMethodSecurity annotation

--- src/main/java/de/schmidtdennis/popupchinese/PopupChineseApplication.java
+++ src/main/java/de/schmidtdennis/popupchinese/PopupChineseApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @SpringBootApplication
 public class PopupChineseApplication {
 
