2fc000a6d78f9eae34c5c2edf07d01cafe31464d
dcnis <bgk.dennis@yahoo.de>
2020-02-29 15:19:51 +0100

    dito

--- src/main/java/de/schmidtdennis/popupchinese/config/WebSecurityConfig.java
+++ src/main/java/de/schmidtdennis/popupchinese/config/WebSecurityConfig.java
@@ -13,19 +13,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    // @Override
-    //     protected void configure(HttpSecurity http) throws Exception {
-    //         http.authorizeRequests()
-    //         // .antMatchers("/findLessonsByDifficulty", "/getLesson/**", "getVocabularyByLessonId/**", "getDialogsByLessonId/**").permitAll()
-    //         .anyRequest().permitAll();
-    //         // .and()
-    //         // .oauth2ResourceServer().jwt();
+    @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+            // .antMatchers("/findLessonsByDifficulty", "/getLesson/**", "getVocabularyByLessonId/**", "getDialogsByLessonId/**").permitAll()
+            .anyRequest().permitAll();
+            // .and()
+            // .oauth2ResourceServer().jwt();
 
-    //         http.cors();
+            http.cors();
 
-    //         // force a non-empty response body for 401's to make the response more browser friendly
-    //         // Okta.configureResourceServer401ResponseBody(http);
+            // force a non-empty response body for 401's to make the response more browser friendly
+            // Okta.configureResourceServer401ResponseBody(http);
 
-    //     }
+        }
         
 }
