914eccf0d3f1f7d4ad66e69a928982cee0ccf3ef
dcnis <bgk.dennis@yahoo.de>
2020-02-29 11:40:25 +0100

    permitAll routes

--- src/main/java/de/schmidtdennis/popupchinese/config/WebSecurityConfig.java
+++ src/main/java/de/schmidtdennis/popupchinese/config/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         protected void configure(HttpSecurity http) throws Exception {
             http.authorizeRequests()
             .antMatchers("/findLessonsByDifficulty", "/getLesson/**", "getVocabularyByLessonId/**", "getDialogsByLessonId/**").permitAll()
-            .anyRequest().authenticated()
+            .anyRequest().permitAll()
             .and()
             .oauth2ResourceServer().jwt();
 
