ff78b573266049022653d65190a416b8988e4d3a
dcnis <bgk.dennis@yahoo.de>
2020-03-26 23:12:03 +0100

    added h2 database and integration test

--- src/test/java/de/schmidtdennis/popupchinese/repository/LessonRepositoryTest.java
+++ src/test/java/de/schmidtdennis/popupchinese/repository/LessonRepositoryTest.java
@@ -1,16 +1,36 @@
 package de.schmidtdennis.popupchinese.repository;
 
+import de.schmidtdennis.popupchinese.data.dto.Lessons;
+import de.schmidtdennis.popupchinese.data.repository.LessonRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
 public class LessonRepositoryTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private LessonRepository lessonRepository;
 
     @Test
     public void test(){
+
+        Lessons lesson = lessonRepository.findById(1);
+
+        assertThat(lesson.getId()).isEqualTo(1);
+        assertThat(lesson.getDifficulty().getDescription()).isEqualTo("Absolute Beginners");
 
     }
 
---
+++ src/test/resources/schema-h2.sql
@@ -0,0 +1,24 @@
+CREATE TABLE difficulties
+(
+    id INT PRIMARY KEY,
+    description VARCHAR(50)
+);
+
+CREATE TABLE lessons
+(
+    id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    discussion VARCHAR(500),
+    difficulty INT,
+    thumbnail VARCHAR(255),
+    FOREIGN KEY (difficulty) REFERENCES difficulties(id)
+);
+
+
+INSERT INTO difficulties (id, description) VALUES (1, 'Absolute Beginners');
+INSERT INTO difficulties (id, description) VALUES (2, 'Elementary');
+INSERT INTO difficulties (id, description) VALUES (3, 'Intermediate');
+INSERT INTO difficulties (id, description) VALUES (4, 'Advanced');
+
+INSERT INTO lessons (id, title, discussion, thumbnail, difficulty)
+VALUES(1, 'My first lesson', 'This is the discussion', 'https://www.thumbnail.com', 1);
